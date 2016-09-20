0a22209dd7f94ab7e9889b7cafa2e6f5170b5e7f
pramayu <gedepramayu@gmail.com>
2016-09-20 17:31:26 +0700

    login

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,12 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 9 }
-
+  
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end 
   
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   resources :events
   resources :users
-  get 'signup', to: 'users#new', as: :user_signup
+  get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   root 'events#index'
 
   # Example of regular route:
